af723ba5337b78da326db6a2681736af30911d1c
Igor Zadubinin <igor.zadubinin@gmail.com>
2021-08-10 08:09:30 +0300

    added javadoc

--- src/test/java/igorzadubinintest/MyBranchingTest.java
+++ src/test/java/igorzadubinintest/MyBranchingTest.java
@@ -13,6 +13,11 @@ public class MyBranchingTest {
     Utils utilsMock;
     MyBranching myBranching;
 
+    /**
+     * Создание mock utilsMock на объект Utils
+     * Создание объекта MyBranching
+     * Установка возвращаемых значений для методов Utils#utilFunc1, Utils#utilFunc2
+     */
     @BeforeEach
     public void init() {
         utilsMock = Mockito.mock(Utils.class);
@@ -21,26 +26,30 @@ public class MyBranchingTest {
         Mockito.when(utilsMock.utilFunc2()).thenReturn(true);
     }
 
+    /**
+     * Проверка результата выполнения метода в зависимости от возможных реализаций {@link Utils}
+     * Должно возвращаться большее из значений или 0, если Utils#utilFunc2 вернёт true;
+     */
     @Test
     public void maxInt_Test() {
         final int i1 = 5, i2 = 10;
 
-        //Func2 = true; Func1 = true
         Assertions.assertTrue(myBranching.maxInt(i1, i2) == 0);
 
         Mockito.when(utilsMock.utilFunc1(anyString())).thenReturn(false);
-        //Func2 = true; Func1 = false
         Assertions.assertTrue(myBranching.maxInt(i1, i2) == 0);
 
         Mockito.when(utilsMock.utilFunc2()).thenReturn(false);
-        //Func2 = false; Func1 = false
         Assertions.assertTrue(myBranching.maxInt(i1, i2) == (i1 >= i2 ? i1 : i2));
 
         Mockito.when(utilsMock.utilFunc1(anyString())).thenReturn(true);
-        //Func2 = false; Func1 = true
         Assertions.assertTrue(myBranching.maxInt(i1, i2) == (i1 >= i2 ? i1 : i2));
     }
 
+    /**
+     * Проверка результата выполнения метода в зависимости от возможных реализаций {@link Utils}
+     * Должно возвращаться true, если Utils#utilFunc2() возвращает true
+     */
     @Test
     public void ifElseExample_Test() {
         Assertions.assertTrue(myBranching.ifElseExample());
@@ -48,24 +57,36 @@ public class MyBranchingTest {
         Assertions.assertFalse(myBranching.ifElseExample());
     }
 
+    /**
+     * Проверка выполнения Utils#utilFunc1 и Utils#utilFunc2 при входящем i = 0
+     */
     @Test
     public void switchExampleI0_Test() {
         myBranching.switchExample(0);
         utilFuncVerify();
     }
 
+    /**
+     * Проверка выполнения Utils#utilFunc1 и Utils#utilFunc2 при входящем i = 1
+     */
     @Test
     public void switchExampleI1_Test() {
         myBranching.switchExample(1);
         utilFuncVerify();
     }
 
+    /**
+     * Проверка выполнения Utils#utilFunc1 и Utils#utilFunc2 при входящем i = 2
+     */
     @Test
     public void switchExampleI2_Test() {
         myBranching.switchExample(2);
         utilFuncVerify();
     }
 
+    /**
+     * Метод для проверки выполнения Utils#utilFunc1 и Utils#utilFunc2
+     */
     public void utilFuncVerify() {
         Mockito.verify(utilsMock).utilFunc1(anyString());
         Mockito.verify(utilsMock).utilFunc2();
